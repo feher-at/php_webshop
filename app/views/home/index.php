fed51e82c24db46a1381d1f38fe9f4608a07013a
makakoocelot <makakoocelot@gmail.com>
2020-08-25 15:44:51 +0200

    Now all the previous function rewrited because of the lack of post and get request handling functions,now we can send post and get request to the server side and we can handle it,but we did not finish it,but we can finish it tomorrow so we will able to start to create our project

---
+++ app/views/home/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+?>
+
+<form action="aboutUs" method="post">
+
+   Küld ezt a fost el pls: <input type="text" name="name"><br>
+    rák id: <input type="text" name="id"><br>
+    <input type="submit">
+</form>
--- public/index.php
+++ public/index.php
@@ -1,17 +1,36 @@
 <?php
-define('ROOT',dirname(__DIR__). DIRECTORY_SEPARATOR);
+/*define('ROOT',dirname(__DIR__). DIRECTORY_SEPARATOR);
 define('APP',ROOT.'app'.DIRECTORY_SEPARATOR);
 define('VIEWS',ROOT.'app'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR);
 define('MODELS',ROOT.'app'.DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR);
 define('DATA',ROOT.'app'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR);
-define('CORE',ROOT.'app'.DIRECTORY_SEPARATOR.'core'.DIRECTORY_SEPARATOR);
+define('CORE',ROOT.'app'.DIRECTORY_SEPARATOR.'Core'.DIRECTORY_SEPARATOR);
 define('CONTROLLERS',ROOT.'app'.DIRECTORY_SEPARATOR.'controllers'.DIRECTORY_SEPARATOR);
 define('SERVICES',ROOT.'app'.DIRECTORY_SEPARATOR.'services'.DIRECTORY_SEPARATOR);
 $modules = [ROOT,APP,CORE,CONTROLLERS,DATA];
 
 set_include_path(get_include_path() .PATH_SEPARATOR . implode(PATH_SEPARATOR,$modules));
 spl_autoload_register('spl_autoload',false);
-var_dump($_SERVER['REQUEST_URI']);
+var_dump($_SERVER['REQUEST_URI']);*/
 
-new Application;
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+
+use app\Core\Application;
+use app\controllers\homeController;
+
+$app = new Application(dirname(__DIR__));
+
+
+$app->router->get('/', 'home');
+
+$app->router->get('/contact','contact');
+
+$app->router->post('/contact',[homeController::class, 'handleContact']);
+
+
+
+
+$app->run();
 ?>
