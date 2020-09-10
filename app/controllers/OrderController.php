875f263807de6726134cfd75d2053c06773bedc3
makakoocelot <makakoocelot@gmail.com>
2020-09-10 19:02:08 +0200

    Change: implement the private makeTheArrayForTheBuyerData function and now on the buyerData page we write out the user transactional data,if the user choosen the transactional option at the order

--- app/controllers/OrderController.php
+++ app/controllers/OrderController.php
@@ -6,6 +6,8 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\Core\Request;
+use app\services\EmailService;
+use app\services\IEmailService;
 use app\services\IItemService;
 use app\services\IOrderService;
 use app\services\IPaymentService;
@@ -23,6 +25,7 @@ class OrderController extends Controller
     private IItemService $itemService;
     private IOrderService $orderService;
     private IPaymentService $paymentService;
+    private IEmailService  $emailService;
 
     public function __construct()
     {
@@ -30,6 +33,7 @@ class OrderController extends Controller
         $this->itemService = new ItemService();
         $this->orderService = new OrderService();
         $this->paymentService = new PaymentService();
+        $this->emailService = new EmailService();
     }
     public function getOrderPage(Request $request)
     {
@@ -39,7 +43,7 @@ class OrderController extends Controller
         $allInfoForOrder['payments'] = $this->paymentService->getAllPaymentPriceAndName($itemId['item_id']);
         $allInfoForOrder['couriers'] = $this->shippingService->getAllCouriersToOneItem($itemId['item_id']);
         $allInfoForOrder['price'] = $this->itemService->getGivenItemCurrentPrice($itemId['item_id']);
-        var_dump($allInfoForOrder);
+
         return $this->render('items/order',$allInfoForOrder);
     }
 
@@ -47,9 +51,14 @@ class OrderController extends Controller
     {
         $body = $request->getBody();
         $this->orderService->createOrder($this->makeOrderArray($body));
-        var_dump($this->makeTheArrayForTheBuyerData($body));
-        return $this->render('items/buyerData');
+        $userUseTransactionData['data'] = array();
+        if($body['payments'] == 'transaction')
+            $userUseTransactionData['data'] = $this->getUserTransactionData($body);
+        return $this->render('items/buyerData',$userUseTransactionData);
 
+    }
+    public function getUsersOrders($userId){
+        return $this->orderService->getAllOrdersOfUser($userId);
     }
 
     private function makeOrderArray($body)
@@ -67,25 +76,34 @@ class OrderController extends Controller
     }
     private function makeTheArrayForTheBuyerData($body)
     {
-        $PaymentPrice = intval($this->paymentService->getGivenItemGivenPaymentPrice($body['item_id'],$body['payments']));
-        $ShippingPrice = intval($this->shippingService->getGivenItemGivenShippingPrice($body['item_id'],$body['payments']));
-        $finalPrice = $PaymentPrice + $ShippingPrice + intval($body['price']);
-        if($body['payments']!= "transaction")
-        {
-            return array('customer_name' =>$body['first_name']." ".$body['last_name'],
-                        'customer_shipping_address' =>$body['customer_shipping_address'],
-                        'customer_billing_address' =>$body['customer_billing_address'],
-                        'customer_phone' =>$body['customer_phone'],
-                        'customer_email' =>$body['customer_email'],
-                        'item_id' =>$body['item_id'],
-                        'item_current_price' =>$body['price'],
-                        'item_quantity' => $body['item_quantity'],
-                        'order_status' => 'order arrived',
-                        'price' => $finalPrice);
-        }
+
+        $paymentPrice = $this->paymentService->getGivenItemGivenPaymentPrice($body['item_id'],$body['payments']);
+        $shippingPrice = $this->shippingService->getGivenItemGivenShippingPrice($body['item_id'],$body['couriers']);
+        $finalPrice = intval($paymentPrice['0']['payment_handlingfee'])
+                    + intval($shippingPrice['0']['shipping_price']) + intval($body['price']);
+
+
+        return array('customer_name' =>$body['first_name']." ".$body['last_name'],
+                    'customer_shipping_address' =>$body['customer_shipping_address'],
+                    'customer_billing_address' =>$body['customer_billing_address'],
+                    'customer_phone' =>$body['customer_phone'],
+                    'customer_email' =>$body['customer_email'],
+                    'item_id' =>$body['item_id'],
+                    'item_current_price' =>$body['price'],
+                    'item_quantity' => $body['item_quantity'],
+                    'order_status' => 'order arrived',
+                    'price' => $finalPrice);
+
     }
-    public function getUsersOrders($userId){
-        return $this->orderService->getAllOrdersOfUser($userId);
+
+    private function getUserTransactionData($body)
+    {
+        return array_slice($body,-3);
+    }
+
+    private function makeTheEmail($dataToTheEmail)
+    {
+
     }
 
 }
